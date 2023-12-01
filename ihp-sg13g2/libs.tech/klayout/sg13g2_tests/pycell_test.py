cdbc05f32c8eb84af717230c38610571a66b0939
ThomasZecha <zecha@ihp-microelectronics.com>
2023-12-01 09:44:04 +0100

    KLayout PyCell integration -added and integrated pmos PyCell -extended tech information for pmos -fixed syntax typo in class Numeric -fixed attribute typo in class Layer -added cmim and pmos to PyCell test-library -removed trailing spaces
    Signed-off-by: ThomasZecha <zecha@ihp-microelectronics.com>

--- ihp-sg13g2/libs.tech/klayout/sg13g2_tests/pycell_test.py
+++ ihp-sg13g2/libs.tech/klayout/sg13g2_tests/pycell_test.py
@@ -1,13 +1,13 @@
 ########################################################################
 #
 # Copyright 2023 IHP PDK Authors
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,12 +28,14 @@
 ly = pya.Layout()
 
 pcellNmos = ly.create_cell("nmos", "IHP PyCells", { "l": 0.350e-6, "w": 6e-6, "ng": 3 })
-#pcellCmim = ly.create_cell("cmim", "IHP PyCells", { "l": "0.350e-6", "w": "6e-6"})
+pcellPmos = ly.create_cell("pmos", "IHP PyCells", { "l": 0.350e-6, "w": 6e-6, "ng": 3 })
+pcellCmim = ly.create_cell("cmim", "IHP PyCells", { "l": "0.350e-6", "w": "6e-6"})
 #pcell = ly.create_cell("rsil", "IHP PyCells", { "l": "3e-6", "w": "6e-6", "b": 1, "ps": "1e-6", "R": "1e-6"})
 
 top = ly.create_cell("TOP")
 top.insert(pya.DCellInstArray(pcellNmos, pya.DTrans()))
-#top.insert(pya.DCellInstArray(pcellCmim, pya.DTrans()))
+top.insert(pya.DCellInstArray(pcellPmos, pya.DTrans(pya.DVector(3, 0))))
+top.insert(pya.DCellInstArray(pcellCmim, pya.DTrans(pya.DVector(0, 3))))
 
 output = "ihp-pycells.gds"
 ly.write(output)
