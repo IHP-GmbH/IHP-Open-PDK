cac2a7887f88e539d146972bdfb28d3b6f1d93ab
Sergei Andreev <116548619+sergeiandreyev@users.noreply.github.com>
2025-03-24 21:20:53 +0100

    Update esd_code.py
    - added file header
    - removed 'Display' parameter
    
    Signed-off-by: Sergei Andreev <116548619+sergeiandreyev@users.noreply.github.com>

--- ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/esd_code.py
+++ ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/esd_code.py
@@ -1,3 +1,21 @@
+########################################################################
+#
+# Copyright 2025 IHP PDK Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+########################################################################
+
 __version__ = "$Revision: #3 $"
 
 from cni.dlo import *
@@ -44,7 +62,6 @@ class esd(DloGen):
 
         model = 'diodevdd_2kv'
         
-        specs('Display', 'Selected', 'Display', ChoiceConstraint(['All', 'Selected']))
         specs('model', model, 'Model name', ChoiceConstraint(['diodevdd_2kv', 'diodevss_2kv', 'diodevdd_4kv', 'diodevss_4kv', 'nmoscl_2', 'nmoscl_4']) )
 
     def setupParams(self, params):
