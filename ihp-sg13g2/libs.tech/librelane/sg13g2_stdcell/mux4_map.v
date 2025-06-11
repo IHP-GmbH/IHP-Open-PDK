e4468ac3794eb61e27b5c87d9e2e44b14f02d425
Leo Moser <leomoser99@gmail.com>
2025-06-11 11:43:11 +0200

    libs.tech/librelane: Add initial LibreLane setup
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

---
+++ ihp-sg13g2/libs.tech/librelane/sg13g2_stdcell/mux4_map.v
@@ -0,0 +1,19 @@
+module \$_MUX4_ (
+    output Y,
+    input A,
+    input B,
+    input C,
+    input D,
+    input S,
+    input T
+    );
+  sg13g2_mux4_1 _TECHMAP_MUX4 (
+      .X(Y),
+      .A0(A),
+      .A1(B),
+      .A2(C),
+      .A3(D),
+      .S0(S),
+      .S1(T)
+  );
+endmodule
