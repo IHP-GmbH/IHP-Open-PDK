9f34b77b86416f75602261547f3554f1aa47d3e9
Martin Jan Köhler <info@martinjankoehler.com>
2025-08-22 21:38:39 +0200

    Add guard_ring PyCell
    Signed-off-by: Martin Jan Köhler <info@martinjankoehler.com>
    Signed-off-by: Martin Jan Köhler <info@martinjankoehler.com>

--- ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/__init__.py
+++ ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/__init__.py
@@ -74,7 +74,8 @@ moduleNames = [
         'SVaricap_code',
         'pnpMPA_code',
         'isolbox_code',
-        'schottky_code'
+        'schottky_code',
+        'guard_ring_code',
 ]
 
 def getProcessNames():
