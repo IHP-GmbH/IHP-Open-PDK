034882c28eadca9c71f8d99a168ae9af7118f466
Martin Jan Köhler <info@martinjankoehler.com>
2025-08-28 16:07:07 +0200

    guard_ring_code: Add missing import
    Signed-off-by: Martin Jan Köhler <info@martinjankoehler.com>

--- ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/guard_ring_code.py
+++ ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/guard_ring_code.py
@@ -17,6 +17,7 @@
 ########################################################################
 
 from __future__ import annotations
+import sys
 from typing import List
 
 __version__ = '$Revision: #3 $'
