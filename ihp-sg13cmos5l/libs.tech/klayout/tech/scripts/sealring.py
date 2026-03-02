050a679d7869a0c937fced6e14d7248ea2088d6d
KrzysztofHerman <herman@ihp-microelectronics.com>
2026-03-02 12:21:53 +0000

    Removing commented libraries and non UTF-8 characters
    Signed-off-by: KrzysztofHerman <herman@ihp-microelectronics.com>
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- ihp-sg13cmos5l/libs.tech/klayout/tech/scripts/sealring.py
+++ ihp-sg13cmos5l/libs.tech/klayout/tech/scripts/sealring.py
@@ -65,7 +65,7 @@ def generate_sealring(width: float, heigth: float, input: str | None, output: st
     pcell = layout.add_pcell_variant(lib, pcell_decl.id(), {'w': f'{width}u', 'l': f'{heigth}u'})
     layout.cell(pcell)
 
-    # Convert offset from µm to dbu
+    # Convert offset from um to dbu
     dx = int(float(offset_x) * 1000)
     dy = int(float(offset_y) * 1000)
 
