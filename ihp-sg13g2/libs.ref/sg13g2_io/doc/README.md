1a29eb4980d520d9ce7273593dc1599310cb81a9
Alexey Balashov <balashov@ihp-microelectronics.com>
2026-05-19 15:57:18 +0200

    I/O update from 19.05.2026
    Signed-off-by: Alexey Balashov <balashov@ihp-microelectronics.com>

--- ihp-sg13g2/libs.ref/sg13g2_io/doc/README.md
+++ ihp-sg13g2/libs.ref/sg13g2_io/doc/README.md
@@ -24,3 +24,21 @@ It also contains externally contributed files:
 * cdl/sg13g2_io.cdl: CDL netlist
 * verilog/sg13g2_io.v: Verilog netlist
 * lib/sg13g2_io_*.lib: Liberty files with timing
+
+19.05.2026  I/O Update 2 resolved issues:
+==========================================================================================
+	  - VIH/VIL input threshold parameters are fixed to be in spec.
+	  - Receiver (input): Delay ballance is fixed.
+	  - Issue #401. Analog Pad current capability is improved.
+	  - Issue #385. M2 connections in ground pads are improved.
+	  - Issue #419. Antenna diode structures were added to core inputs (layout fix).
+	  - Isssue #835. Layout of edges of cells is aligned to provide abuttment without DRC errors.
+	  - Issue #676. Re-CZ is done. New look-up tabes with increased loads are used. Fillers and corner are added in Liberty and verilog model.
+	  - Issue #909. Contacts are added in Filler200 cell.
+	  - Celllist id updated: subblocks are not library cells.
+	  - "Galery" top assambly is removed from GDS 2 layout view.
+	  - OA database update: schematics, symbols, pin order, CDF term-order properties. Pin direction is updated to correspond verilog model.
+	  - CDL netlist is regenerated and renamed to sg13g2_io.spice.
+	  - SPICE netlist is regenerated.
+	  - LEF is synchronized with open source library. Pin direction in LEF is fixed.
+	   
--- ihp-sg13g2/libs.ref/sg13g2_io/verilog/sg13g2_io.v
+++ ihp-sg13g2/libs.ref/sg13g2_io/verilog/sg13g2_io.v
@@ -15,374 +15,567 @@
 // type: Corner
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_Corner (iovdd, iovss, vdd, vss);
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_Corner
+//   cell_description : Physical I/O filler - corner.
+//*****************************************************************
+module sg13g2_Corner (vdd, vss, iovdd, iovss);
+	inout vdd;
+	inout vss;	
 	inout iovdd;
 	inout iovss;
-	inout vdd;
-	inout vss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Filler200
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_Filler200 (iovdd, iovss, vdd, vss);
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_Filler200
+//   cell_description : Physical I/O filler width 1 um.
+//*****************************************************************
+module sg13g2_Filler200 (vdd, vss, iovdd, iovss);
+	inout vdd;
+	inout vss;	
 	inout iovdd;
 	inout iovss;
-	inout vdd;
-	inout vss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Filler400
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_Filler400 (iovdd, iovss, vdd, vss);
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_Filler400
+//   cell_description : Physical I/O filler width 2 um.
+//*****************************************************************
+module sg13g2_Filler400 (vdd, vss, iovdd, iovss);
+	inout vdd;
+	inout vss;	
 	inout iovdd;
 	inout iovss;
-	inout vdd;
-	inout vss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Filler1000
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_Filler1000 (iovdd, iovss, vdd, vss);
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_Filler1000
+//   cell_description : Physical I/O filler width 5 um.
+//*****************************************************************
+module sg13g2_Filler1000 (vdd, vss, iovdd, iovss);
+	inout vdd;
+	inout vss;	
 	inout iovdd;
 	inout iovss;
-	inout vdd;
-	inout vss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Filler2000
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_Filler2000 (iovdd, iovss, vdd, vss);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_Filler2000
+//   cell_description : Physical I/O filler width 10 um.
+//*****************************************************************
+module sg13g2_Filler2000 (vdd, vss, iovdd, iovss);
 	inout vdd;
 	inout vss;
+	inout iovdd;
+	inout iovss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Filler4000
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_Filler4000 (iovdd, iovss, vdd, vss);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_Filler4000
+//   cell_description : Physical I/O filler width 20 um.
+//*****************************************************************
+module sg13g2_Filler4000 (vdd, vss, iovdd, iovss);
 	inout vdd;
 	inout vss;
+	inout iovdd;
+	inout iovss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Filler10000
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_Filler10000 (iovdd, iovss, vdd, vss);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_Filler10000
+//   cell_description : Physical I/O filler width 50 um.
+//*****************************************************************
+module sg13g2_Filler10000 (vdd, vss, iovdd, iovss);
 	inout vdd;
 	inout vss;
+	inout iovdd;
+	inout iovss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Input
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadIn (iovdd, iovss, vdd, vss, pad, p2c);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadIn
+//   cell_description : 3.3V CMOS input GPIO buffer.
+//*****************************************************************
+module sg13g2_IOPadIn (pad, p2c, vdd, vss, iovdd, iovss);
+	input pad;
+	output p2c;
 	inout vdd;
 	inout vss;
-	inout pad;
-	output p2c;
-
+	inout iovdd;
+	inout iovss;
+	
 	// Function
 	assign p2c = pad;
 
 	// Timing
+
 	specify
-		(pad => p2c) = 0;
+		(posedge pad => (p2c : pad)) = (0.0,0.0);
+		(negedge pad => (p2c : pad)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Output4mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadOut4mA (iovdd, iovss, vdd, vss, pad, c2p);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadOut4mA
+//   cell_description : 3.3V CMOS 4 mA output GPIO buffer.
+//*****************************************************************
+module sg13g2_IOPadOut4mA (pad, c2p, vdd, vss, iovdd, iovss);
+	output pad;
+	input c2p;
 	inout vdd;
 	inout vss;
-	inout pad;
-	input c2p;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = c2p;
 
 	// Timing
+
 	specify
-		(c2p => pad) = 0;
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Output16mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadOut16mA (iovdd, iovss, vdd, vss, pad, c2p);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadOut16mA
+//   cell_description : 3.3V CMOS 16 mA output GPIO buffer.
+//*****************************************************************
+module sg13g2_IOPadOut16mA (pad, c2p, vdd, vss, iovdd, iovss);
+	output pad;
+	input c2p;
 	inout vdd;
 	inout vss;
-	inout pad;
-	input c2p;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = c2p;
 
 	// Timing
+
 	specify
-		(c2p => pad) = 0;
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Output30mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadOut30mA (iovdd, iovss, vdd, vss, pad, c2p);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadOut30mA
+//   cell_description : 3.3V CMOS 30 mA output GPIO buffer.
+//*****************************************************************
+module sg13g2_IOPadOut30mA (pad, c2p, vdd, vss, iovdd, iovss);
+	output pad;
+	input c2p;
 	inout vdd;
 	inout vss;
-	inout pad;
-	input c2p;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = c2p;
 
 	// Timing
+
 	specify
-		(c2p => pad) = 0;
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: TriStateOutput4mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadTriOut4mA (iovdd, iovss, vdd, vss, pad, c2p, c2p_en);
-	inout iovdd;
-	inout iovss;
-	inout vdd;
-	inout vss;
-	inout pad;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadTriOut4mA
+//   cell_description : 3.3V CMOS 4 mA output GPIO buffer with tri-state option.
+//*****************************************************************
+module sg13g2_IOPadTriOut4mA (pad, c2p, c2p_en, vdd, vss, iovdd, iovss);
+	output pad;
 	input c2p;
 	input c2p_en;
+	inout vdd;
+	inout vss;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = (c2p_en) ? c2p : 1'bz;
 
 	// Timing
+
 	specify
-		if (c2p_en == 1'b1)
-			(c2p => pad) = 0;
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
+		(posedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: TriStateOutput16mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadTriOut16mA (iovdd, iovss, vdd, vss, pad, c2p, c2p_en);
-	inout iovdd;
-	inout iovss;
-	inout vdd;
-	inout vss;
-	inout pad;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadTriOut16mA
+//   cell_description : 3.3V CMOS 16 mA output GPIO buffer with tri-state option.
+//*****************************************************************
+module sg13g2_IOPadTriOut16mA (pad, c2p, c2p_en, vdd, vss, iovdd, iovss);
+	output pad;
 	input c2p;
 	input c2p_en;
+	inout vdd;
+	inout vss;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = (c2p_en) ? c2p : 1'bz;
 
 	// Timing
+
 	specify
-		if (c2p_en == 1'b1)
-			(c2p => pad) = 0;
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
+		(posedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: TriStateOutput30mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadTriOut30mA (iovdd, iovss, vdd, vss, pad, c2p, c2p_en);
-	inout iovdd;
-	inout iovss;
-	inout vdd;
-	inout vss;
-	inout pad;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadTriOut30mA
+//   cell_description : 3.3V CMOS 30 mA output GPIO buffer with tri-state option.
+//*****************************************************************
+module sg13g2_IOPadTriOut30mA (pad, c2p, c2p_en, vdd, vss, iovdd, iovss);
+	output pad;
 	input c2p;
 	input c2p_en;
+	inout vdd;
+	inout vss;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = (c2p_en) ? c2p : 1'bz;
 
 	// Timing
+
 	specify
-		if (c2p_en == 1'b1)
-			(c2p => pad) = 0;
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
+		(posedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: InputOutput4mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadInOut4mA (iovdd, iovss, vdd, vss, pad, c2p, c2p_en, p2c);
-	inout iovdd;
-	inout iovss;
-	inout vdd;
-	inout vss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadInOut4mA
+//   cell_description : 3.3V CMOS 4 mA bidirectional GPIO buffer with tri-state option.
+//*****************************************************************
+module sg13g2_IOPadInOut4mA (pad, c2p, p2c, c2p_en, vdd, vss, iovdd, iovss);
 	inout pad;
 	input c2p;
-	input c2p_en;
 	output p2c;
+	input c2p_en;
+	inout vdd;
+	inout vss;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = (c2p_en) ? c2p : 1'bz;
 	assign p2c = pad;
 
 	// Timing
+
 	specify
-		if (c2p_en == 1'b1)
-			(c2p => pad) = 0;
-		(pad => p2c) = 0;
+		(posedge pad => (p2c : pad)) = (0.0,0.0);
+		(negedge pad => (p2c : pad)) = (0.0,0.0);
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
+		(posedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: InputOutput4mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadInOut16mA (iovdd, iovss, vdd, vss, pad, c2p, c2p_en, p2c);
-	inout iovdd;
-	inout iovss;
-	inout vdd;
-	inout vss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadInOut16mA
+//   cell_description : 3.3V CMOS 16 mA bidirectional GPIO buffer with tri-state option.
+//*****************************************************************
+module sg13g2_IOPadInOut16mA (pad, c2p, p2c, c2p_en, vdd, vss, iovdd, iovss);
 	inout pad;
 	input c2p;
-	input c2p_en;
 	output p2c;
+	input c2p_en;
+	inout vdd;
+	inout vss;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = (c2p_en) ? c2p : 1'bz;
 	assign p2c = pad;
 
 	// Timing
+
 	specify
-		if (c2p_en == 1'b1)
-			(c2p => pad) = 0;
-		(pad => p2c) = 0;
+		(posedge pad => (p2c : pad)) = (0.0,0.0);
+		(negedge pad => (p2c : pad)) = (0.0,0.0);
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
+		(posedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: InputOutput4mA
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadInOut30mA (iovdd, iovss, vdd, vss, pad, c2p, c2p_en, p2c);
-	inout iovdd;
-	inout iovss;
-	inout vdd;
-	inout vss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadInOut30mA
+//   cell_description : 3.3V CMOS 30 mA bidirectional GPIO buffer with tri-state option.
+//*****************************************************************
+module sg13g2_IOPadInOut30mA (pad, c2p, p2c, c2p_en, vdd, vss, iovdd, iovss);
 	inout pad;
 	input c2p;
-	input c2p_en;
 	output p2c;
+	input c2p_en;
+	inout vdd;
+	inout vss;
+	inout iovdd;
+	inout iovss;
 
 	// Function
 	assign pad = (c2p_en) ? c2p : 1'bz;
 	assign p2c = pad;
 
 	// Timing
+
 	specify
-		if (c2p_en == 1'b1)
-			(c2p => pad) = 0;
-		(pad => p2c) = 0;
+		(posedge pad => (p2c : pad)) = (0.0,0.0);
+		(negedge pad => (p2c : pad)) = (0.0,0.0);
+		(posedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p => (pad : c2p)) = (0.0,0.0);
+		(negedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
+		(posedge c2p_en => (pad:c2p_en)) = (0.0,0.0);
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Analog
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadAnalog (iovdd, iovss, vdd, vss, pad, padres);
-	inout iovdd;
-	inout iovss;
-	inout vdd;
-	inout vss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadAnalog
+//   cell_description : 0 - 3.3V 27 mA analog pad (600 uA "padres" low-power input-to-gate analog bus).
+//*****************************************************************
+module sg13g2_IOPadAnalog (pad, padres, vdd, vss, iovdd, iovss);
 	inout pad;
 	inout padres;
-
+	inout vdd;
+	inout vss;	
+	inout iovdd;
+	inout iovss;
+	
 	// Function
 	assign pad = padres;
 	assign padres = pad;
 
 	// Timing
+
 	specify
-		(pad => padres) = 0;
-		(padres => pad) = 0;
 	endspecify
+
 endmodule
 `endcelldefine
 
 // type: IOVss
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadIOVss (iovdd, iovss, vdd, vss);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadIOVss
+//   cell_description : External IO ground pad.
+//*****************************************************************
+module sg13g2_IOPadIOVss (vdd, vss, iovdd, iovss);
 	inout vdd;
 	inout vss;
+	inout iovdd;
+	inout iovss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: IOVdd
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadIOVdd (iovdd, iovss, vdd, vss);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadIOVdd
+//   cell_description : External IO 3.3V supply pad.
+//*****************************************************************
+module sg13g2_IOPadIOVdd (vdd, vss, iovdd, iovss);
 	inout vdd;
 	inout vss;
+	inout iovdd;
+	inout iovss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Vss
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadVss (iovdd, iovss, vdd, vss);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadVss
+//   cell_description : Core ground pad.
+//*****************************************************************
+module sg13g2_IOPadVss (vdd, vss, iovdd, iovss);
 	inout vdd;
 	inout vss;
+	inout iovdd;
+	inout iovss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
 
 // type: Vdd
 `timescale 1ns/10ps
 `celldefine
-module sg13g2_IOPadVdd (iovdd, iovss, vdd, vss);
-	inout iovdd;
-	inout iovss;
+//*****************************************************************
+//   technology       : SG13G2
+//   module name      : sg13g2_IOPadVdd
+//   cell_description : Core 1.2V supply pad.
+//*****************************************************************
+module sg13g2_IOPadVdd (vdd, vss, iovdd, iovss);
 	inout vdd;
 	inout vss;
+	inout iovdd;
+	inout iovss;
+
+	specify
+	endspecify
+
 endmodule
 `endcelldefine
