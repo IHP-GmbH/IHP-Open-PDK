2c9a372059665a52d57d9ffaebe94fa6bec9a26f
Martin Jan Köhler <info@martinjankoehler.com>
2026-07-15 18:29:22 +0200

    Support native PCell for 'o' tool
    Signed-off-by: Martin Jan Köhler <info@martinjankoehler.com>
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- ihp-sg13cmos5l/libs.tech/klayout/python/sg13cmos5l_pycell_lib/__init__.py
+++ ihp-sg13cmos5l/libs.tech/klayout/python/sg13cmos5l_pycell_lib/__init__.py
@@ -26,6 +26,7 @@ from cni.dlo import PCellWrapper
 # Creates the SG13_dev technology
 from .sg13_tech import *
 from .sg13_tech_info import *
+from .native_pcells.via_pcell import ViaPCell
 
 from pypreprocessor.pypreprocessor import preprocessor as preProcessor
 
@@ -248,6 +249,15 @@ class PyCellLib(pya.Library):
 
         self.register("SG13_dev")
 
-# instantiate and register the library
-PyCellLib()
 
+class SG13G2_NativePCellLib(pya.Library):
+    def __init__(self):
+        self.description = "SG13G2 Native PCells"
+        self.technology = SG13_Tech.TECH_NAME
+        self.layout().register_pcell("Via", ViaPCell())
+        self.register("SG13_native_pcell_lib")
+
+
+# instantiate and register the libraries
+PyCellLib()
+SG13G2_NativePCellLib()
