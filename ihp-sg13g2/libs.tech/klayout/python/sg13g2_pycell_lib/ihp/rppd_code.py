8c3f394bd2d8e99ab18487038a3fa8411ec90c96
Martin Jan Köhler <info@martinjankoehler.com>
2026-09-18 17:25:52 +0200

    rppd: clean up imports
    Signed-off-by: Martin Jan Köhler <info@martinjankoehler.com>

--- ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/rppd_code.py
+++ ihp-sg13g2/libs.tech/klayout/python/sg13g2_pycell_lib/ihp/rppd_code.py
@@ -18,10 +18,9 @@
 __version__ = '$Revision: #3 $'
 
 from cni.dlo import *
-from .res_base_code import *
-from .thermal import *
 from .geometry import *
 from .guard_ring_code import GuardRingType
+from .res_base_code import *
 from .thermal import *
 from .utility_functions import *
 
